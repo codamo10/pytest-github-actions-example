5e8534ae0d34b335400f272e8deccc82c1ab784d
codamo10 <codamo10@gmail.com>
2024-12-10 19:00:58 -0600

    Second commit

--- src/area.py
+++ src/area.py
@@ -3,6 +3,7 @@ def calculate_area_square(length: int | float) -> int | float:
     Function to calculate the area of a square
     :param length: length of the square
     :return: area of the square
+    Testing
     """
     if not isinstance(length, (int, float)) or length <= 0:
         raise TypeError("Length must be a positive non-zero number")
